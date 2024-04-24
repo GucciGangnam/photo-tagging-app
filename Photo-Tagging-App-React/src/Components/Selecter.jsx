68b2e894af3d27fd411b40327a550dcbe5060419
Gucci Gangnam <diviner02hive@icloud.com>
2024-04-24 15:22:49 +0700

    front end runnign nicely. NEXT: create DB and then come back to turn setTimeouts to fetch functions

--- Photo-Tagging-App-React/src/Components/Selecter.jsx
+++ Photo-Tagging-App-React/src/Components/Selecter.jsx
@@ -10,39 +10,112 @@ import './Selecter.css'
 
 
 
-export const Selecter = ({selecterVisible, setSelecterVisible, setCircleShowing}) => {
+export const Selecter = ({ selectedCell,
+    setSelectedCell,
+    selecterVisible,
+    setSelecterVisible,
+    setCircleShowing,
+    selectedCharacter,
+    setSelectedCharacter,
+    tomFound,
+    setTomFound,
+    spidermanFound,
+    setSpidermanFound,
+    kennyFound,
+    setKennyFound,
+    rogerFound,
+    setRogerFound,
+    brianFound,
+    setBrianFound
+}) => {
 
-// States 
-
+    // States 
+    const [loading, setLoading] = useState(false)
 
     // Button handlers 
+    // Handle Character select
+
+
+    const handleSelectTom = (e) => { 
+        setSelectedCharacter(e.target.alt)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false);
+            setTomFound(true);
+        }, 2000)
+    }
+    const handleSelectSpiderman = (e) => { 
+        setSelectedCharacter(e.target.alt)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false);
+            setSpidermanFound(true);
+        }, 2000)
+    }
+    const handleSelectKenny = (e) => { 
+        setSelectedCharacter(e.target.alt)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false);
+            setKennyFound(true);
+        }, 2000)
+    }
+    const handleSelectRoger = (e) => { 
+        setSelectedCharacter(e.target.alt)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false);
+            setRogerFound(true);
+        }, 2000)
+    }
+    const handleSelectBrian = (e) => { 
+        setSelectedCharacter(e.target.alt)
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false);
+            setBrianFound(true);
+        }, 2000)
+    }
+
+    // Close selecter
     const handleCloseSeleter = () => {
         setSelecterVisible(false)
+        setLoading(false)
         setCircleShowing('none')
+        setSelectedCell('')
+        setSelectedCharacter('')
     }
 
     return (
-        <div className={selecterVisible ? 'SelecterVisible' : 'Selecter'}>
-
+        <div className={selecterVisible ? 'SelecterVisible' : 'Selecter'} >
             <div className='Selecter-Controlls-island'>
-                <div className='Selecter-Character-Bubble'>
-                    <img src='/PngItem_26692.png' alt='Tom'></img>
-                </div>
-                <div className='Selecter-Character-Bubble'>
-                    <img src='/pngwing.com-2.png' alt='Spiderman'></img>
-                </div>
-                <div className='Selecter-Character-Bubble'>
-                    <img src='/pngwing.com.png' alt='Kenny'></img>
-                </div>
-                <div className='Selecter-Character-Bubble'>
-                    <img src='/Roger-American-Dad-PNG-Photos.png' alt='Roger'></img>
-                </div>
-                <div className='Selecter-Character-Bubble'>
-                    <img src='/pngwing.com-3.png' alt=''></img>
-                </div>
-                <button onClick={handleCloseSeleter} className='Selecter-Close-BTN' style={{ display: selecterVisible ? 'block' : 'none' }}>Close</button>
+                {loading ? <img src='/Loading.gif' alt='Loadig'></img> : (
+                    <>
+                        <div className='Selecter-Character-Bubble'>
+                            <img style={tomFound ? { filter: 'grayscale(100%)' } : null} src='/PngItem_26692.png' alt='Tom' onClick={selecterVisible ? handleSelectTom : null} />
+                            {tomFound && <img style={{ position: "absolute" }} src='/Check Mark.png' alt='Check Mark'></img>}
+                        </div>
+                        <div className='Selecter-Character-Bubble'>
+                            <img style={spidermanFound ? { filter: 'grayscale(100%)' } : null} src='/pngwing.com-2.png' alt='Spiderman' onClick={selecterVisible ? handleSelectSpiderman : null} />
+                            {spidermanFound && <img style={{ position: "absolute" }} src='/Check Mark.png' alt='Check Mark'></img>}
+                        </div>
+                        <div className='Selecter-Character-Bubble'>
+                            <img style={kennyFound ? { filter: 'grayscale(100%)' } : null} src='/pngwing.com.png' alt='Kenny' onClick={selecterVisible ? handleSelectKenny : null} />
+                            {kennyFound && <img style={{ position: "absolute" }} src='/Check Mark.png' alt='Check Mark'></img>}
+                        </div>
+                        <div className='Selecter-Character-Bubble'>
+                            <img style={rogerFound ? { filter: 'grayscale(100%)' } : null} src='/Roger-American-Dad-PNG-Photos.png' alt='Roger' onClick={selecterVisible ? handleSelectRoger : null} />
+                            {rogerFound && <img style={{ position: "absolute" }} src='/Check Mark.png' alt='Check Mark'></img>}
+                        </div>
+                        <div className='Selecter-Character-Bubble'>
+                            <img style={brianFound ? { filter: 'grayscale(100%)' } : null} src='/pngwing.com-3.png' alt='Brian' onClick={selecterVisible ? handleSelectBrian : null} />
+                            {brianFound && <img style={{ position: "absolute" }} src='/Check Mark.png' alt='Check Mark'></img>}
+                        </div>
+                        <button onClick={handleCloseSeleter} className='Selecter-Close-BTN' style={{ display: selecterVisible ? 'block' : 'none' }}>Hide</button>
+                    </>
+                )}
             </div>
-            Response from server - Right or wrong
         </div>
     )
 }
+
--- Photo-Tagging-App-React/src/Pages/Gamepage.jsx
+++ Photo-Tagging-App-React/src/Pages/Gamepage.jsx
@@ -18,17 +18,23 @@ export const Gamepage = ({ setGameState }) => {
 
 
 
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-
-
-
-
 
     // STATES
     // Game function States 
     const [selectedCell, setSelectedCell] = useState('')
+    const [selectedCharacter, setSelectedCharacter] = useState('') 
 
+    const [tomFound, setTomFound] = useState(false);
+    const [spidermanFound, setSpidermanFound] = useState(false);
+    const [kennyFound, setKennyFound] = useState(false);
+    const [rogerFound, setRogerFound] = useState(false);
+    const [brianFound, setBrianFound] = useState(false);
+    
+    // DLEETE ME - UE TO LOG SLEECETD CHARACTER ON CHANGE STATE //
+    useEffect(() => { 
+        console.log("UE char " + selectedCharacter)
+        console.log("UE cell " + selectedCell)
+    }, [selectedCharacter, selectedCell])
     // Selecter States
     const [selecterVisible, setSelecterVisible] = useState(false);
 
@@ -49,11 +55,8 @@ export const Gamepage = ({ setGameState }) => {
         // Open up the selecter
         setSelecterVisible(true)
 
-
-
-
-        // Set the selected grid cell ID number (and surrounding cells) 
-        console.log(e.target.id)
+        // Set selected Cell for backend
+        setSelectedCell(e.target.id)
 
     };
 
@@ -72,7 +75,25 @@ export const Gamepage = ({ setGameState }) => {
             {/* Hightlight circle */}
             <Circle circlePageX={circlePageX} circlePageY={circlePageY} circleShowing={circleShowing} />
             {/* drop down selecter */}
-            <Selecter setCircleShowing={setCircleShowing} selecterVisible={selecterVisible} setSelecterVisible={setSelecterVisible}/>
+            <Selecter 
+            selectedCell={selectedCell} 
+            selectedCharacter={selectedCharacter} 
+            setSelectedCharacter={setSelectedCharacter} 
+            setSelectedCell={setSelectedCell} 
+            setCircleShowing={setCircleShowing} 
+            selecterVisible={selecterVisible} 
+            setSelecterVisible={setSelecterVisible}
+            tomFound={tomFound}
+            setTomFound={setTomFound}
+            spidermanFound={spidermanFound}
+            setSpidermanFound={setSpidermanFound}
+            kennyFound={kennyFound}
+            setKennyFound={setKennyFound}
+            rogerFound={rogerFound}
+            setRogerFound={setRogerFound}
+            brianFound={brianFound}
+            setBrianFound={setBrianFound}
+            />
             {/* game image */}
             <img className='GameIMG' src='/Game image.jpg' alt='Game image'></img>
             {/* cell grids */}
